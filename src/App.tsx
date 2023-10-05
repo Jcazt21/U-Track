00336e9570d1fc117e7b1b4625fafb99f51ab4c5
Jean Azar <jeancarlosazar@gmail.com>
2023-10-05 11:35:42 -0400

    Table Beta

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,32 @@ import React from "react";
 import LoginForm from "./components/LoginForm";
 import HomePage from "./HomePage";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // Importa Link
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <div className="header">
-        <h1>U-Track</h1>
-        <img src="public/logo.png" alt="logo" className="logo" />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="header">
+                  <h1>U-Track</h1>
+                  <img src="public/logo.png" alt="logo" className="logo" />
+                  <Link to="/HomePage" className="btn">
+                    Ir a HomePage
+                  </Link>
+                </div>
+                <LoginForm />
+              </>
+            }
+          />
+          <Route path="/HomePage" element={<HomePage />} />
+        </Routes>
       </div>
-      <div className="Router">
-        <Router>
-          <Routes>
-            <Route path="/" element={<LoginForm />} />
-            <Route path="/HomePage" element={<HomePage />} />
-          </Routes>
-        </Router>
-      </div>
-    </div>
+    </Router>
   );
 }
 
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,12 +2,41 @@ import React from "react";
 import Sidebar from "./components/Sidebar"; // Importa el componente del sidebar
 import Mnsj from "./components/Mnsj";
 import "./HomePage.css";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const HomePage: React.FC = () => {
   return (
     <div className="home-page">
       <Sidebar />
-      <div className="content">{/* <Mnsj /> */}</div>
+      <div className="Table">
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
