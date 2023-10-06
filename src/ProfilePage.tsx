73ed087caf094d775b0578b0dceef7b5aba0f208
Jean Azar <jeancarlosazar@gmail.com>
2023-10-06 08:44:44 -0400

    json-profile

--- src/ProfilePage.tsx
+++ src/ProfilePage.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import "./ProfilePage.css";
+import { Link } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import { Link } from "react-router-dom"; // Importa el componente Link
+import "./ProfilePage.css";
+import users from "./components/user.json"; // Asegúrate de que la ruta al archivo JSON es correcta
 
 const ProfilePage: React.FC = () => {
+  const user = users[0]; // Esto asume que quieres mostrar el primer usuario en el array. Ajusta según sea necesario.
+
   return (
     <div className="profile-container">
       <div className="profile-info">
         <img src="public/prof.png" alt="Profile" className="profile-picture" />
-        <h2 className="name">Nombre</h2>
-        <p className="id">ID: 123456</p>
-        <p className="career">Carrera: Ingeniería en Sistemas</p>
-        <p className="rol">Estudiante</p>
+        <h2 className="name">{user.name}</h2>{" "}
+        <p className="id">ID: {user.id}</p>{" "}
+        <p className="career">Carrera: {user.career}</p>{" "}
+        <p className="rol">{user.rol}</p>{" "}
       </div>
       <Link to="/SupportPage" className="soporte-link">
-        {" "}
-        {/* Agrega el componente Link aquí */}
         <h3 className="soporte">Soporte</h3>
       </Link>
       <Sidebar />
