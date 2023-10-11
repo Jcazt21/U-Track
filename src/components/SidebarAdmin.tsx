d5165a81fc28a9678636ff4af484e77d334a71e8
Jean Azar <jeancarlosazar@gmail.com>
2023-10-10 20:38:10 -0400

    Sidebar Prof y Admin

---
+++ src/components/SidebarAdmin.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom"; // Importa Link de react-router-dom
+import "./Sidebar.css";
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <h2 className="sidebar-title">U-Track</h2>
+        <ul className="list-unstyled">
+          <li>
+            <Link to="/HomePage">
+              {" "}
+              {/* Cambia <a> por <Link> y href por to */}
+              <i className="fas fa-home"></i> Panel
+            </Link>
+          </li>
+          <li>
+            <Link to="/ProfilePage">
+              <i className="fas fa-user"></i> Gestionar Perfil
+            </Link>
+          </li>
+          <li>
+            <Link to="/AsignaturasPage">
+              {" "}
+              {/* Cambia <a> por <Link> y href por to */}
+              <i className="fas fa-book"></i> Gestionar Asignaturas
+            </Link>
+          </li>
+        </ul>
+        <Link to="/">
+          {" "}
+          {/* Agrega esta línea */}
+          <button className="logout">Cerrar Sesion</button>
+        </Link>{" "}
+        {/* Agrega esta línea */}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
