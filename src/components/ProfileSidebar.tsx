6e3c311e0e3f73921f33b4944d80616934d1b8ae
Chacal2002 <guillermorafaelgildiaz@gmail.com>
2023-10-12 23:47:29 -0400

    Integracion SIDE estudiantes arreglada

--- src/components/ProfileSidebar.tsx
+++ src/components/ProfileSidebar.tsx
@@ -1,40 +1,39 @@
 import React, { useEffect, useState } from "react";
 import "./ProfileSidebar.css";
-import estudiantes from "./estudiantes.json"; // Asegúrate de que la ruta al archivo JSON es correcta
 import PieChart from "./PieChart"; // Asegúrate de reemplazar './PieChart' con la ruta real al archivo
 
+interface EstudianteInfo {
+  Nombre_Estudiante: string;
+  ID_Estudiante: number;
+  Indice: number;
+  // ... otros campos según sea necesario
+}
+
 const ProfileSidebar: React.FC = () => {
-  const [estudiante, setEstudiante] = useState<any>(null);
+  const [estudiante, setEstudiante] = useState<EstudianteInfo | null>(null);
 
   useEffect(() => {
-    const usuarioId = localStorage.getItem("usuarioId");
+    const userInfo = localStorage.getItem("userInfo");
 
-    if (usuarioId) {
-      const estudianteEncontrado = estudiantes.estudiantes.find(
-        (est) => est.usuarioId.toString() === usuarioId
-      );
-
-      if (estudianteEncontrado) {
-        setEstudiante(estudianteEncontrado);
-      }
+    if (userInfo) {
+      const estudianteInfo = JSON.parse(userInfo) as EstudianteInfo;
+      setEstudiante(estudianteInfo);
     }
   }, []);
 
   if (!estudiante) return null;
 
-  const { nombre, usuarioId, indice, imagen } = estudiante;
+  const indiceCalculado = (estudiante.Indice * 4) / 100;
 
   return (
     <div className="profile-sidebar-container">
       <div className="profile-sidebar">
-        <div className="profile-picture">
-          <img src={imagen || "public/prof.png"} alt="User Profile" />
-        </div>
-        <h2 className="username">{nombre}</h2>
-        <p className="institutional-id">{usuarioId}</p>
-        <p className="indiceG">{indice}</p>
+        {/* ... otros elementos */}
+        <h2 className="username">{estudiante.Nombre_Estudiante}</h2>
+        <p className="institutional-id">{estudiante.ID_Estudiante}</p>
+        <p className="indiceG">{indiceCalculado.toFixed(2)}</p> {/* Asegúrate de ajustar la precisión según sea necesario */}
         <div className="pie-chart-container">
-          <PieChart indice={indice} /> {/* Pasar el índice como una prop */}
+          <PieChart indice={indiceCalculado} /> {/* Pasar el índice calculado como una prop */}
         </div>
       </div>
     </div>
